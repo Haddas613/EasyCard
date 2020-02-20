559262981ee572e05e8177a002b6bdf0f041a4d5
Vlad Volkov <volkovv@digicode.net>
2020-02-20 10:26:13 +0200

    AuthorizationKeysApiController

---
+++ EasyCard/TransactionsApi/Controllers/AuthorizationKeysApiController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
+using TransactionsApi.Models;
+
+namespace TransactionsApi.Controllers
+{
+    [Route("api/authorizationKeys")]
+    [ApiController]
+    public class AuthorizationKeysApiController : ControllerBase
+    {
+        [HttpPost]
+        [Route("reset")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AuthorizationKeys))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(OperationResponse))]
+        public async Task<IActionResult> ResetKeys()
+        {
+            // TODO: implementation
+            var keys = new AuthorizationKeys
+            {
+                PrivateKey = Guid.NewGuid().ToString(),
+                PublicKey = Guid.NewGuid().ToString()
+            };
+
+            return new JsonResult(keys);
+        }
+    }
+}
---
+++ EasyCard/TransactionsApi/Models/AuthorizationKeys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransactionsApi.Models
+{
+    public class AuthorizationKeys
+    {
+        public string PublicKey { get; set; }
+
+        public string PrivateKey { get; set; }
+    }
+}
