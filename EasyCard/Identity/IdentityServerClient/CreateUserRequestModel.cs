17c5ecc6723bfcf163df5ff83a0a2a65673e8cab
Igor Kanivets <kanivetsig@mydigicode.com>
2020-02-24 18:27:21 +0200

    UserApiController: mocks and tests base

--- EasyCard/Identity/IdentityServerClient/CreateUserRequestModel.cs
+++ EasyCard/Identity/IdentityServerClient/CreateUserRequestModel.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Shared.Models.Identity
+namespace IdentityServerClient
 {
     public class CreateUserRequestModel
     {
---
+++ EasyCard/Tests/MerchantsApi.Tests/UserControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using MerchantsApi.Tests.Fixtures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Xunit.Extensions.Ordering;
+
+namespace MerchantsApi.Tests
+{
+    [Collection("MerchantsCollection"), Order(3)]
+    public class UserControllerTests
+    {
+        private MerchantsFixture merchantsFixture;
+
+        public UserControllerTests(MerchantsFixture merchantsFixture)
+        {
+            this.merchantsFixture = merchantsFixture;
+        }
+
+       
+    }
+}
