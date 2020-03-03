743f06d2cdc564b736c0a88c9db4e9ced2b86971
Vlad Volkov <volkovv@digicode.net>
2020-03-03 11:44:01 +0200

    email sender draft

--- EasyCard/BasicServices/EventHubEmailSender.cs
+++ EasyCard/BasicServices/EventHubEmailSender.cs
@@ -1,14 +1,45 @@
-﻿using Shared.Helpers.Email;
+﻿using Microsoft.Azure.EventHubs;
+using Newtonsoft.Json;
+using Shared.Helpers.Email;
 using System;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace BasicServices
 {
-    public class EventHubEmailSender : IEmailSender
+    /// <summary>
+    /// EmailSender based on Azure EventHub
+    /// </summary>
+    /// <remarks>
+    /// Note: use as singleton
+    /// </remarks>
+    public class EventHubEmailSender : IEmailSender, IDisposable
     {
-        public Task SendEmail(Email email)
+        private readonly EventHubClient eventHubClient;
+
+        public EventHubEmailSender(string eventHubConnectionString, string eventHubName)
         {
-            throw new NotImplementedException();
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder(eventHubConnectionString)
+            {
+                EntityPath = eventHubName
+            };
+
+            this.eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+        }
+
+        public void Dispose()
+        {
+            if (eventHubClient != null)
+            {
+                eventHubClient.Close();
+            }
+        }
+
+        public async Task SendEmail(Email email)
+        {
+            var message = JsonConvert.SerializeObject(email);
+
+            await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)), DateTime.UtcNow.ToString("yy-MM-dd"));
         }
     }
 }
