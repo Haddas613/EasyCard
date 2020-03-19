2de5a5fb0e9818cd02d50b7ec833c97b58de7165
Vlad Volkov <volkovv@digicode.net>
2020-03-19 11:45:41 +0200

    models corrections

---
+++ EasyCard/Transactions.Api/Models/Transactions/CreditCardDetails.cs
@@ -0,0 +1,23 @@
+﻿using Shared.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Transactions.Api.Models.Transactions
+{
+    /// <summary>
+    /// Do not store full card number. Please use 123456****1234 pattern CreditCardHelpers.GetCardDigits()
+    /// </summary>
+    public class CreditCardDetails : CreditCardDetailsBase
+    {
+        public string CardOwnerName { get; set; }
+
+        /// <summary>
+        /// Stored token reference
+        /// </summary>
+        public string CardToken { get; set; }
+
+        public bool? IsTourist { get; set; }
+    }
+}
