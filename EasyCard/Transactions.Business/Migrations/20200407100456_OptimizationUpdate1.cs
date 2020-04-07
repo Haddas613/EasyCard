edaf8d709e03d9509124e51694011a4645306fe3
Igor Kanivets <kanivetsig@mydigicode.com>
2020-04-07 13:17:59 +0300

    ECNG-129: Transactions Database update

---
+++ EasyCard/Transactions.Business/Migrations/20200407100456_OptimizationUpdate1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Transactions.Business.Migrations
+{
+    public partial class OptimizationUpdate1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdditionalDetails",
+                table: "TransactionHistory");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CardOwnerName",
+                table: "PaymentTransaction",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldUnicode: false,
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AdditionalDetails",
+                table: "TransactionHistory",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CardOwnerName",
+                table: "PaymentTransaction",
+                type: "varchar(100)",
+                unicode: false,
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
