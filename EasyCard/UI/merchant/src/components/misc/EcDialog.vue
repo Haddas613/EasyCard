6c54c7d540dc79c0abc75c500d511cf05fab53a1
Igor Kanivets <kanivetsig@mydigicode.com>
2020-07-10 15:17:11 +0300

    Alternative design completed

---
+++ EasyCard/UI/merchant/src/components/misc/EcDialog.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-dialog v-model="dialog" fullscreen>
+    <v-card>
+      <v-card-title class="headline px-2 py-0">
+        <v-row :align="'center'">
+          <v-col class="d-flex justify-start px-1" cols="2" md="3" lg="4">
+            <v-icon @click="$emit('update:dialog', false)" class="px-2">mdi-arrow-left</v-icon>
+          </v-col>
+          <v-col class="d-flex justify-space-around">
+            <slot name="title"></slot>
+          </v-col>
+          <v-col cols="2" md="3" lg="4" class="d-flex justify-end">
+          </v-col>
+        </v-row>
+      </v-card-title>
+      <v-divider></v-divider>
+      <v-card-text class="px-0">
+        <v-content class="py-2">
+          <v-container fluid ma-0 pa-0 center>
+            <v-row no-gutters v-bind:class="{'pt-2': $vuetify.breakpoint.lgAndUp}">
+              <v-col cols="3" class="hidden-sm-and-down">
+                <v-spacer></v-spacer>
+              </v-col>
+              <v-col class="d-flex justify-space-around">
+                <slot></slot>
+              </v-col>
+              <v-col cols="3" class="hidden-sm-and-down">
+                <v-spacer></v-spacer>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-content>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+/** slots: title & default (body) */
+export default {
+  props: {
+    dialog: {
+      type: Boolean,
+      default: false,
+      required: true
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- EasyCard/UI/merchant/src/layouts/wizard/Index.vue
+++ EasyCard/UI/merchant/src/layouts/wizard/Index.vue
@@ -1,8 +1,5 @@
 <template>
   <v-app id="inspire" v-bind:dir="$vuetify.rtl ? 'rtl' : 'ltr'">
-    <wizzard-app-bar :drawer.sync="drawer" />
-
-    <ec-nav :drawer.sync="drawer" />
     <wizard-content />
   </v-app>
 </template>
@@ -15,8 +12,6 @@ export default {
 
   components: {
     WizardContent: () => import("./Content.vue"),
-    WizzardAppBar: () => import("./AppBar.vue"),
-    EcNav: () => import("../shared/EcNav.vue")
   },
 
   data: () => ({
