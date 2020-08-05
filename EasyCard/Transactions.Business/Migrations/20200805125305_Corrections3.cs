24415c79ea771c694dcf96deab236d4b1e39a0e1
Vlad Volkov <volkovv@digicode.net>
2020-08-05 17:57:10 +0300

    corrections

---
+++ EasyCard/Transactions.Business/Migrations/20200805125305_Corrections3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Transactions.Business.Migrations
+{
+    public partial class Corrections3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "BillingDealID",
+                table: "PaymentTransaction",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "BillingDealID",
+                table: "PaymentTransaction",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+    }
+}
