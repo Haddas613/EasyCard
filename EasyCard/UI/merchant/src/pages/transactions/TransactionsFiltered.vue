57e8d88fb69f2c38cc1ca5aab9a10db3f6f7b2b0
Igor Kanivets <kanivetsig@mydigicode.com>
2020-09-16 10:29:50 +0300

    MerchantUI: transaction & billing deals date format update

--- EasyCard/UI/merchant/src/pages/transactions/TransactionsFiltered.vue
+++ EasyCard/UI/merchant/src/pages/transactions/TransactionsFiltered.vue
@@ -57,7 +57,7 @@
               lg="6"
               class="pt-1 caption ecgray--text"
             >{{item.paymentTransactionID}}</v-col>
-            <v-col cols="12" md="6" lg="6">{{item.$transactionTimestamp | ecdate('MM/DD/YYYY HH:mm')}}</v-col>
+            <v-col cols="12" md="6" lg="6">{{item.$transactionTimestamp | ecdate('DD/MM/YYYY HH:mm')}}</v-col>
           </template>
 
           <template v-slot:right="{ item }">
