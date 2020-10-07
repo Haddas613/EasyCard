96150505df76cfabfc440652a6da8b2410523691
Igor Kanivets <kanivetsig@mydigicode.com>
2020-10-07 11:37:10 +0300

    ECNG-294: Items in $ display fix

--- EasyCard/UI/merchant/src/extensions/filters/currency.js
+++ EasyCard/UI/merchant/src/extensions/filters/currency.js
@@ -1,15 +1,21 @@
 import store from '../../store/index';
 
-export default function(value, currency) {
+export default function (value, currency) {
     value = parseFloat(value);
     if (typeof value !== "number") {
         return value;
     }
-    var formatter = new Intl.NumberFormat(store.state.localization.currentLocale, {
+    let loc = store.state.localization.currentLocale;
+    if (loc == "en-IL") {
+        //en-IL shows USD as US$ which is not acceptable
+        loc = "en-US";
+    }
+
+    var formatter = new Intl.NumberFormat(loc, {
         style: 'currency',
         currency: currency,
         minimumFractionDigits: 2
     });
-    
+
     return formatter.format(value);
 };
