11b027500e9f84b400942886aa6c60f16c18e413
Igor Kanivets <kanivetsig@mydigicode.com>
2020-11-30 14:23:38 +0200

    Payment request link width fix

--- EasyCard/UI/merchant/src/pages/payment-requests/PaymentRequestInfo.vue
+++ EasyCard/UI/merchant/src/pages/payment-requests/PaymentRequestInfo.vue
@@ -43,7 +43,9 @@
                 {{$t('PaymentRequestURL')}} 
                 <v-chip class="mb-1" @click="$copyToClipboard(model.paymentRequestUrl)" x-small color="primary">{{$t("Copy")}}</v-chip>
               </p>
-              <a role="button" target="_blank" class="d-flex primary--text text-oneline" :href="model.paymentRequestUrl">{{model.paymentRequestUrl}}</a>
+              <a role="button" target="_blank" :href="model.paymentRequestUrl">
+                <small>{{model.paymentRequestUrl}}</small>
+              </a>
             </v-col>
           </v-row>
         </v-card-text>
