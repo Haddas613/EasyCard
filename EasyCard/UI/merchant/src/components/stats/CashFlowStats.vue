19b1585350c7ca8f64e872236d685ac3089df439
Igor Kanivets <kanivetsig@mydigicode.com>
2020-12-03 16:05:00 +0200

    ECNG-421: Cash flow line chart stub

--- EasyCard/UI/merchant/src/components/stats/CashFlowStats.vue
+++ EasyCard/UI/merchant/src/components/stats/CashFlowStats.vue
@@ -7,16 +7,94 @@
       </v-row>
     </v-card-title>
     <v-divider></v-divider>
-    <v-card-text>
-      <v-row align="center" justify="center" class="py-4">placeholder</v-row>
+    <v-card-text class="px-0">
+      <v-container class="chart-container px-0">
+        <line-chart v-if="draw" :options="chartOptions" :data="chartData"></line-chart>
+      </v-container>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
-export default {};
+export default {
+  components: {
+    LineChart: () => import("../charts/LineChart")
+  },
+  data() {
+    return {
+      draw: true, //set to true when ready to draw chart
+      chartOptions: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                //min: 0,
+                //max: 1500,
+                // stepSize: 500,
+                // display: false,
+                maxTicksLimit: 3,
+              },
+              gridLines: {
+                drawOnChartArea: true,
+                drawBorder: false,
+                drawTicks: false,
+                //offsetGridLines: true
+              },
+              afterTickToLabelConversion: function(scaleInstance) {
+                // set the first and last tick to null so it does not display
+                // note, ticks[0] is the last tick and ticks[length - 1] is the first
+                scaleInstance.ticks[scaleInstance.ticks.length - 1] = null;
+
+                // need to do the same thing for this similiar array which is used internally
+                scaleInstance.ticksAsNumbers[scaleInstance.ticksAsNumbers.length - 1] = null;
+              },
+            }
+          ],
+          xAxes: [
+            {
+              gridLines: {
+                drawOnChartArea: false,
+                drawTicks: false,
+                display:false,
+              }
+            }
+          ]
+        }
+      },
+      chartData: {
+        labels: ["Jul", "Aug", "Sep"],
+        datasets: [
+          {
+            pointRadius: 0,
+            borderColor: '#fd8442bf',
+            label: "Data One",
+            backgroundColor: '#fd84420d',
+            // backgroundColor: '#ffa53269',
+            data: [250, 750, 500]
+          }
+        ]
+      }
+    };
+  },
+  mounted () {
+    // console.log(document.querySelector('.chart-container'))
+    // var ctx = document.getElementById('line-chart').getContext("2d");
+    
+    // var gradientStroke = ctx.createLinearGradient(500, 0, 100, 0);
+    // gradientStroke.addColorStop(0, '#80b6f4');
+    // gradientStroke.addColorStop(1, '#f49080');
+  },
+};
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+.chart-container{
+  flex-grow: 1;
+  min-height: 0;
 
+  > div {
+    position: relative;
+    height: 250px;
+  }
+}
 </style>
