de2d5a39a84a20919e77a49e9ab455e3151615ec
Vlad Volkov <volkovv@mydigicode.com>
2021-02-10 11:37:20 +0200

    refresh database structure

--- EasyCard/Data/Merchants.Database/dbo/Tables/Terminal.sql
+++ EasyCard/Data/Merchants.Database/dbo/Tables/Terminal.sql
@@ -1,20 +1,25 @@
 ﻿CREATE TABLE [dbo].[Terminal] (
-    [TerminalID]             UNIQUEIDENTIFIER NOT NULL,
-    [MerchantID]             UNIQUEIDENTIFIER NOT NULL,
-    [Label]                  NVARCHAR (50)    NOT NULL,
-    [UpdateTimestamp]        ROWVERSION       NULL,
-    [Status]                 INT              NOT NULL,
-    [ActivityStartDate]      DATETIME2 (7)    NULL,
-    [Created]                DATETIME2 (7)    NULL,
-    [BillingSettings]        NVARCHAR (MAX)   NULL,
-    [CheckoutSettings]       NVARCHAR (MAX)   NULL,
-    [InvoiceSettings]        NVARCHAR (MAX)   NULL,
-    [PaymentRequestSettings] NVARCHAR (MAX)   NULL,
-    [Settings]               NVARCHAR (MAX)   NULL,
-    [SharedApiKey]           VARBINARY (64)   NULL,
+    [TerminalID]                  UNIQUEIDENTIFIER NOT NULL,
+    [MerchantID]                  UNIQUEIDENTIFIER NOT NULL,
+    [Label]                       NVARCHAR (50)    NOT NULL,
+    [UpdateTimestamp]             ROWVERSION       NULL,
+    [Status]                      INT              NOT NULL,
+    [ActivityStartDate]           DATETIME2 (7)    NULL,
+    [Created]                     DATETIME2 (7)    NULL,
+    [BillingSettings]             NVARCHAR (MAX)   NULL,
+    [CheckoutSettings]            NVARCHAR (MAX)   NULL,
+    [InvoiceSettings]             NVARCHAR (MAX)   NULL,
+    [PaymentRequestSettings]      NVARCHAR (MAX)   NULL,
+    [Settings]                    NVARCHAR (MAX)   NULL,
+    [SharedApiKey]                VARBINARY (64)   NULL,
+    [EnabledFeatures]             VARCHAR (MAX)    NULL,
+    [AggregatorTerminalReference] VARCHAR (50)     NULL,
+    [ProcessorTerminalReference]  VARCHAR (50)     NULL,
     CONSTRAINT [PK_Terminal] PRIMARY KEY CLUSTERED ([TerminalID] ASC),
     CONSTRAINT [FK_Terminal_Merchant_MerchantID] FOREIGN KEY ([MerchantID]) REFERENCES [dbo].[Merchant] ([MerchantID])
 );
+
+
 
 
 
--- EasyCard/Data/Merchants.Database/dbo/Tables/UserTerminalMapping.sql
+++ EasyCard/Data/Merchants.Database/dbo/Tables/UserTerminalMapping.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[UserTerminalMapping] (
     [UserTerminalMappingID] BIGINT           IDENTITY (1, 1) NOT NULL,
     [UserID]                UNIQUEIDENTIFIER NOT NULL,
-    [TerminalID]            UNIQUEIDENTIFIER NOT NULL,
+    [TerminalID]            UNIQUEIDENTIFIER NULL,
     [OperationDate]         DATETIME2 (7)    NOT NULL,
     [OperationDoneBy]       NVARCHAR (50)    NULL,
     [OperationDoneByID]     UNIQUEIDENTIFIER NULL,
@@ -9,9 +9,12 @@
     [Email]                 NVARCHAR (50)    NULL,
     [Roles]                 VARCHAR (MAX)    NULL,
     [MerchantID]            UNIQUEIDENTIFIER DEFAULT ('00000000-0000-0000-0000-000000000000') NOT NULL,
+    [Status]                SMALLINT         DEFAULT (CONVERT([smallint],(0))) NOT NULL,
     CONSTRAINT [PK_UserTerminalMapping] PRIMARY KEY CLUSTERED ([UserTerminalMappingID] ASC),
     CONSTRAINT [FK_UserTerminalMapping_Terminal_TerminalID] FOREIGN KEY ([TerminalID]) REFERENCES [dbo].[Terminal] ([TerminalID])
 );
+
+
 
 
 
