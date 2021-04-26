35e6f8e8e2e1ddeb0f28137ce3b5f31419f51389
Vlad Volkov <volkovv@mydigicode.com>
2021-04-26 17:28:51 +0300

    database project correction

--- EasyCard/Data/Merchants.Database/dbo/Tables/Impersonation_1.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Impersonation] (
-    [UserId]     UNIQUEIDENTIFIER NOT NULL,
-    [MerchantID] UNIQUEIDENTIFIER NOT NULL,
-    CONSTRAINT [PK_Impersonation] PRIMARY KEY CLUSTERED ([UserId] ASC)
-);
-
