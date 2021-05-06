1c81c2313a29a68a6852efad7f60ebbc516929aa
Igor Kanivets <kanivetsig@mydigicode.com>
2021-05-06 15:44:59 +0300

    ECNG-712: Implement OnlyActual filter

--- EasyCard/UI/merchant/src/pages/billing-deals/BillingDealsList.vue
+++ EasyCard/UI/merchant/src/pages/billing-deals/BillingDealsList.vue
@@ -57,7 +57,7 @@
     </v-card>
     <v-card width="100%" flat :loading="!billingDeals">
       <v-card-text class="px-0 pt-0">
-        <ec-list :items="billingDeals" v-if="billingDeals" :dense="true">
+        <ec-list :items="billingDeals" v-if="billingDeals">
           <template v-slot:prepend="{ item }" v-if="billingDealsFilter.onlyActual">
             <div class="px-1">
               <v-checkbox v-model="item.selected" v-if="!item.processed"></v-checkbox>
@@ -111,10 +111,10 @@
               lg="6"
               class="pt-1 caption ecgray--text"
             >
-            <span v-if="item.$nextScheduledTransaction" 
+            <b v-if="item.$nextScheduledTransaction" 
               v-bind:class="{'error--text': (item.$nextScheduledTransaction > now)}">
-              {{item.$nextScheduledTransaction | ecdate('DD/MM/YYYY HH:mm')}}
-            </span>
+              {{item.$nextScheduledTransaction | ecdate('DD/MM/YYYY')}}
+            </b>
             <span v-else>-</span>
             </v-col>
             <v-col cols="12" md="6" lg="6">{{item.cardOwnerName || '-'}}</v-col>
