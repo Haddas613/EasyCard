c44d0881dceaf27d2ca2141082e4ee1f148bf400
Igor Kanivets <kanivetsig@mydigicode.com>
2021-05-18 16:05:44 +0300

    ECNG-727: Transaction History: Add dialog (like in system logs)

--- EasyCard/UI/admin/src/components/transactions/TransactionHistory.vue
+++ EasyCard/UI/admin/src/components/transactions/TransactionHistory.vue
@@ -1,5 +1,23 @@
 <template>
   <v-flex fill-height>
+    <ec-dialog :dialog.sync="showDetailsDialog">
+      <template v-slot:title>{{$t('Transaction')}}</template>
+      <template>
+        <div v-if="selectedHistory" class="body-1 black--text">
+          <h3>
+            {{selectedHistory.$timestamp | ecdate}}
+          </h3>
+          <p class="pt-1">{{$t('CorrelationID')}}: <b>{{selectedHistory.correlationId}}</b></p>
+          <v-switch :label="$t('FormatCode')" v-model="preMode"></v-switch>
+          <div class="py-2 body-2">
+            <code v-if="preMode">
+              <pre>{{selectedHistory.operationDescription}}</pre>
+            </code>
+            <p v-else>{{selectedHistory.operationDescription}}</p>
+          </div>
+        </div>
+      </template>
+    </ec-dialog>
     <v-simple-table>
       <template v-slot:default>
         <thead>
@@ -16,7 +34,11 @@
             <td>{{ item.operationDate | ecdate}}</td>
             <td>{{ item.operationDoneBy }}</td>
             <td>{{ item.operationCode }}</td>
-            <td>{{ item.operationDescription }}</td>
+            <td>
+              <span class="cursor-pointer primary--text" @click="showDialog(item)">
+                {{item.operationDescription | length(100)}}...
+              </span>
+            </td>
             <td>{{ item.operationMessage }}</td>
           </tr>
         </tbody>
@@ -28,7 +50,8 @@
 <script>
 export default {
   components: {
-    EcList: () => import("../ec/EcList")
+    EcList: () => import("../ec/EcList"),
+    EcDialog: () => import("../ec/EcDialog"),
   },
   props: {
     transactionId: {
@@ -38,7 +61,10 @@ export default {
   },
   data() {
       return {
-          items: []
+        showDetailsDialog: false,
+        selectedHistory: null,
+        preMode: true,
+        items: []
       }
   },
   async mounted(){
@@ -46,6 +72,12 @@ export default {
       if (history && history.numberOfRecords > 0){
           this.items = history.data;
       }
-  }
+  },
+  methods: {
+    showDialog(transactionHistory) {
+      this.selectedHistory = transactionHistory;
+      this.showDetailsDialog = true;
+    }
+  },
 };
 </script>
