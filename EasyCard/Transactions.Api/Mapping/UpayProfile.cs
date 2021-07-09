cd00e46d016babfb07acf05c1711605e8ded4375
Vlad Volkov <volkovv@digicode.net>
2021-07-09 21:17:38 +0300

    correction

--- EasyCard/Transactions.Api/Mapping/UpayProfile.cs
+++ EasyCard/Transactions.Api/Mapping/UpayProfile.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Transactions.Api.Models.External;
 using Transactions.Api.Models.Tokens;
 using Transactions.Api.Models.Transactions;
 using Transactions.Business.Entities;
@@ -45,6 +46,25 @@ namespace Transactions.Api.Mapping
                  .ForMember(m => m.ShvaShovarNumber, src => src.MapFrom(f => f.ShvaShovarNumber))
                  .ForMember(m => m.ShvaAuthNum, src => src.MapFrom(f => f.ShvaAuthNum))
                  .ForMember(m => m.ShvaTransmissionNumber, src => src.MapFrom(f => f.ShvaTransmissionNumber));
+
+            CreateMap<PaymentTransaction, UpayValidateDealResult>()
+               .ForMember(d => d.Amount, o => o.MapFrom(src => src.Amount))
+               .ForMember(d => d.CardCompany, o => o.MapFrom(src => src.CreditCardDetails.CardVendor))
+               .ForMember(d => d.CardNumber, o => o.MapFrom(src => CreditCardHelpers.GetCardLastFourDigits(src.CreditCardDetails.CardNumber)))
+               .ForMember(d => d.CardOwner, o => o.MapFrom(src => src.CreditCardDetails.CardOwnerName))
+               .ForMember(d => d.CardReader, o => o.MapFrom(src => src.CardPresence))
+               .ForMember(d => d.CardType, o => o.MapFrom(src => src.CreditCardDetails.CardBrand))
+               .ForMember(d => d.ExpiryDate, o => o.MapFrom(src => CreditCardHelpers.ParseCardExpiration(src.CreditCardDetails.CardExpiration)))
+               .ForMember(d => d.ExternalID, o => o.MapFrom(src => src.PaymentRequestID))
+               .ForMember(d => d.FirstPayment, o => o.MapFrom(src => src.InitialPaymentAmount))
+               .ForMember(d => d.ForeignCard, o => o.MapFrom(src => CreditCardHelpers.ParseCardExpiration(src.CreditCardDetails.CardExpiration)))
+               .ForMember(d => d.IDNumber, o => o.MapFrom(src => src.CreditCardDetails.CardOwnerNationalID))
+               .ForMember(d => d.ManpikID, o => o.MapFrom(src => src.ShvaTransactionDetails.Solek))
+               .ForMember(d => d.NumPayments, o => o.MapFrom(src => src.NumberOfPayments))
+               .ForMember(d => d.OkNumber, o => o.MapFrom(src => src.ShvaTransactionDetails.ShvaAuthNum))
+               .ForMember(d => d.OtherPayment, o => o.MapFrom(src => src.InitialPaymentAmount))
+               .ForMember(d => d.SixDigits, o => o.MapFrom(src => CreditCardHelpers.GetCardBin(src.CreditCardDetails.CardNumber)))
+               .ForMember(d => d.Terminal, o => o.MapFrom(src => src.ShvaTransactionDetails.ShvaTerminalID));
         }
     }
 }
