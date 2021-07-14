8c1dc35da963081bc5bd0ed740fb1bd102a2a4c0
Vlad Volkov <volkovv@mydigicode.com>
2021-07-14 17:09:25 +0300

    migration added

---
+++ EasyCard/Transactions.Business/Migrations/20210714140545_LegacyQueryString.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Transactions.Business.Migrations
+{
+    public partial class LegacyQueryString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EmvSoftVersion",
+                table: "PaymentTransaction",
+                type: "varchar(20)",
+                unicode: false,
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompRetailerNum",
+                table: "PaymentTransaction",
+                type: "varchar(20)",
+                unicode: false,
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PinPadDeviceID",
+                table: "PaymentTransaction",
+                type: "varchar(20)",
+                unicode: false,
+                maxLength: 20,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PinPadDeviceID",
+                table: "PaymentTransaction");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmvSoftVersion",
+                table: "PaymentTransaction",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldUnicode: false,
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CompRetailerNum",
+                table: "PaymentTransaction",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldUnicode: false,
+                oldMaxLength: 20,
+                oldNullable: true);
+        }
+    }
+}
