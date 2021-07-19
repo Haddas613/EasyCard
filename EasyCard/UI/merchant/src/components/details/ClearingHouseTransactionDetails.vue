15b1fbca3b49cc0bd86a8b16fd854e339f568f92
Igor Kanivets <kanivetsig@mydigicode.com>
2021-07-19 10:39:35 +0300

    ECNG-817: Transaction Details - Clearing House transaction id fix

--- EasyCard/UI/merchant/src/components/details/ClearingHouseTransactionDetails.vue
+++ EasyCard/UI/merchant/src/components/details/ClearingHouseTransactionDetails.vue
@@ -8,7 +8,7 @@
       <v-row class="info-container body-1 black--text">
         <v-col cols="12" md="4" class="info-block">
           <p class="caption ecgray--text text--darken-2">{{$t('ClearingHouseTransactionID')}}</p>
-          <p>{{model.сlearingHouseTransactionID || '-'}}</p>
+          <p>{{model.clearingHouseTransactionID || '-'}}</p>
         </v-col>
         <v-col cols="12" md="4" class="info-block">
           <p class="caption ecgray--text text--darken-2">{{$t('MerchantReference')}}</p>
