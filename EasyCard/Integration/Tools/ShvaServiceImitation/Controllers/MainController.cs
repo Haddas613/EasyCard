84b8439fc3145bc55847002aa38d84a7cd6fae44
Vlad Volkov <volkovv@mydigicode.com>
2021-07-29 13:15:52 +0300

    more cases for emulator

--- EasyCard/Integration/Tools/ShvaServiceImitation/Controllers/MainController.cs
+++ EasyCard/Integration/Tools/ShvaServiceImitation/Controllers/MainController.cs
@@ -57,7 +57,7 @@ namespace ShvaServiceImitation.Controllers
         {
             var response = new AshAuthResponseBody();
             response.pinpad = new clsPinPad();
-            if (body.inputObj.carNumber.Contains("532610") || body.inputObj.carNumber.EndsWith("0443"))
+            if (body.inputObj.carNumber.Contains("532610") || body.inputObj.carNumber.EndsWith("0443") || body.inputObj.carNumber == "5105105105105100")
             {
                 int[] numbers = new int[4] { 3, 4, 7, 9 };
                 Random rd = new Random();
