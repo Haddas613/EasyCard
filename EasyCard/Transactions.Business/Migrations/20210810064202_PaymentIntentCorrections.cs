28d9a49d0f24b3d771ac4c0e45f3c96207a13452
Vlad Volkov <volkovv@mydigicode.com>
2021-08-10 09:45:05 +0300

    migration

---
+++ EasyCard/Transactions.Business/Migrations/20210810064202_PaymentIntentCorrections.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Transactions.Business.Migrations
+{
+    public partial class PaymentIntentCorrections : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "UserAmount",
+                table: "PaymentRequest",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserAmount",
+                table: "PaymentRequest");
+        }
+    }
+}
