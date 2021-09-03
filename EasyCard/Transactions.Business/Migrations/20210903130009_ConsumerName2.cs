0d48dca061e7765bd90cc7738a86139bc17e4c2c
Vlad Volkov <volkovv@mydigicode.com>
2021-09-03 16:06:20 +0300

    migration added

---
+++ EasyCard/Transactions.Business/Migrations/20210903130009_ConsumerName2.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Transactions.Business.Migrations
+{
+    public partial class ConsumerName2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConsumerNationalID",
+                table: "PaymentTransaction",
+                type: "varchar(20)",
+                unicode: false,
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConsumerNationalID",
+                table: "PaymentRequest",
+                type: "varchar(20)",
+                unicode: false,
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConsumerNationalID",
+                table: "Invoice",
+                type: "varchar(20)",
+                unicode: false,
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConsumerNationalID",
+                table: "BillingDeal",
+                type: "varchar(20)",
+                unicode: false,
+                maxLength: 20,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConsumerNationalID",
+                table: "PaymentTransaction");
+
+            migrationBuilder.DropColumn(
+                name: "ConsumerNationalID",
+                table: "PaymentRequest");
+
+            migrationBuilder.DropColumn(
+                name: "ConsumerNationalID",
+                table: "Invoice");
+
+            migrationBuilder.DropColumn(
+                name: "ConsumerNationalID",
+                table: "BillingDeal");
+        }
+    }
+}
