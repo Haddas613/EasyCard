ba1a83335ea320d571dc0861d6d3398806eed88b
Igor Kanivets <kanivetsig@mydigicode.com>
2021-10-28 16:42:36 +0300

    ECNG-1064: Transaction VAT recalculation if mismatch

--- EasyCard/Transactions.Api/Extensions/DealDetailsExtensions.cs
+++ EasyCard/Transactions.Api/Extensions/DealDetailsExtensions.cs
@@ -65,13 +65,16 @@ namespace Transactions.Api.Extensions
                 };
             }
 
-            if (dealDetails.Items?.Count() > 0 && dealDetails.Items?.Sum(i => i.Amount).GetValueOrDefault(0) != financialItem.Amount)
+            if (dealDetails.Items?.Count() > 0 &&
+                (dealDetails.Items?.Sum(i => i.Amount).GetValueOrDefault(0) != financialItem.Amount
+                || dealDetails.Items?.Sum(i => i.VAT).GetValueOrDefault(0) != financialItem.VATTotal))
             {
                 var firstItem = dealDetails.Items.First();
 
                 firstItem.Amount = financialItem.Amount - dealDetails.Items?.Sum(i => i.Amount).GetValueOrDefault(0);
                 firstItem.Price = firstItem.Amount;
-                firstItem.VAT = Math.Round(firstItem.Amount.Value / (1m + financialItem.VATRate), 2, MidpointRounding.AwayFromZero);
+                //firstItem.VAT = Math.Round(firstItem.Amount.Value / (1m + financialItem.VATRate), 2, MidpointRounding.AwayFromZero);
+                firstItem.VAT = financialItem.VATTotal - dealDetails.Items?.Sum(i => i.VAT).GetValueOrDefault(0);
                 firstItem.NetAmount = firstItem.Amount - firstItem.VAT;
             }
 
