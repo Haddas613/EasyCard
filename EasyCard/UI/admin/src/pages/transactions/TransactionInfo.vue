41861346906d376ca89ece8a81305529d3aeb730
Igor Kanivets <kanivetsig@mydigicode.com>
2021-11-01 13:36:33 +0200

    ECNG-1084: Add rejection message to UI

--- EasyCard/UI/admin/src/pages/transactions/TransactionInfo.vue
+++ EasyCard/UI/admin/src/pages/transactions/TransactionInfo.vue
@@ -145,6 +145,10 @@
                   <p class="caption ecgray--text text--darken-2">{{$t('RejectionReason')}}</p>
                   <p>{{model.rejectionReason}}</p>
                 </v-col>
+                <v-col cols="12" md="4" class="info-block">
+                  <p class="caption ecgray--text text--darken-2">{{$t('RejectionMessage')}}</p>
+                  <p>{{model.rejectionMessage}}</p>
+                </v-col>
               </v-row>
             </v-card-text>
           </v-card>
