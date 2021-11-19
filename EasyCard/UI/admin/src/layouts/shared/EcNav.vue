4d808a1ad7fbe2fb6cb00cd1c3dd06544e195dcd
Igor Kanivets <kanivetsig@mydigicode.com>
2021-11-19 13:26:42 +0200

    ECNG-1127: AdminUI: Reorder terminals/merchants menu

--- EasyCard/UI/admin/src/layouts/shared/EcNav.vue
+++ EasyCard/UI/admin/src/layouts/shared/EcNav.vue
@@ -126,6 +126,16 @@ export default {
               to: { name: "Merchants" }
             },
             {
+              icon: "mdi-plus",
+              text: "CreateMerchant",
+              to: { name: "CreateMerchant" }
+            },
+            {
+              icon: "mdi-format-list-bulleted",
+              text: "TerminalsList",
+              to: { name: "Terminals" }
+            },
+            {
               icon: "mdi-account-network",
               text: "UsersList",
               to: { name: "Users" }
@@ -134,11 +144,6 @@ export default {
               icon: "mdi-credit-card-settings",
               text: "CreditCardTokens",
               to: { name: "CardTokens" }
-            },
-            {
-              icon: "mdi-plus",
-              text: "CreateMerchant",
-              to: { name: "CreateMerchant" }
             }
           ]
         },
@@ -148,11 +153,6 @@ export default {
           text: "Terminals",
           expanded: false,
           children: [
-            {
-              icon: "mdi-format-list-bulleted",
-              text: "TerminalsList",
-              to: { name: "Terminals" }
-            },
             {
               icon: "mdi-format-list-bulleted-type",
               text: "TerminalTemplates",
