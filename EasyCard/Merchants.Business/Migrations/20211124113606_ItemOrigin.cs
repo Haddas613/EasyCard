bf4e2d9d1bb5400df3b79e8e9b8343c6ef924133
Vlad Volkov <volkovv@mydigicode.com>
2021-11-24 13:46:07 +0200

    item origin db

---
+++ EasyCard/Merchants.Business/Migrations/20211124113606_ItemOrigin.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Merchants.Business.Migrations
+{
+    public partial class ItemOrigin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Active",
+                table: "Item",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Origin",
+                table: "Item",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Active",
+                table: "Consumer",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Origin",
+                table: "Item");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Active",
+                table: "Item",
+                type: "bit",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Active",
+                table: "Consumer",
+                type: "bit",
+                nullable: false,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
