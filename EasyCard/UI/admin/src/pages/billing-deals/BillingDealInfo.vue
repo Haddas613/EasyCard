a5f2d048d3160f19868d0edd48f4099946abcb6e
Igor Kanivets <kanivetsig@mydigicode.com>
2021-12-22 11:50:36 +0200

    ECNG-1244: AdminUI: Add terminal link to billing deal

--- EasyCard/UI/admin/src/pages/billing-deals/BillingDealInfo.vue
+++ EasyCard/UI/admin/src/pages/billing-deals/BillingDealInfo.vue
@@ -31,7 +31,11 @@
                 </v-col>
                 <v-col cols="12" md="4" class="info-block">
                   <p class="caption ecgray--text text--darken-2">{{$t('Terminal')}}</p>
-                  <p>{{model.terminalName}}</p>
+                  <p>
+                    <router-link link :to="{name: 'EditTerminal', params: {id: model.$terminalID || model.terminalID}}">
+                      {{model.terminalName}}
+                    </router-link>
+                  </p>
                 </v-col>
                 <v-col cols="12" md="4" class="info-block">
                   <p class="caption ecgray--text text--darken-2">{{$t('Created')}}</p>
