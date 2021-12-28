965ae953d180a30f825df9e3567859b49690c278
Igor Kanivets <kanivetsig@mydigicode.com>
2021-12-28 11:05:33 +0200

    ECNG-1263: Please sync validation between front and back for bank details

--- EasyCard/Merchants.Shared/Models/TerminalBankDetails.cs
+++ EasyCard/Merchants.Shared/Models/TerminalBankDetails.cs
@@ -18,7 +18,7 @@ namespace Merchants.Shared.Models
         [Required(AllowEmptyStrings = false)]
         public string BankAccount { get; set; }
 
-        [StringLength(8, MinimumLength = 8)]
+        [StringLength(9, MinimumLength = 3)]
         public string InstituteNum { get; set; }
 
         public int? InstituteServiceNum { get; set; }
