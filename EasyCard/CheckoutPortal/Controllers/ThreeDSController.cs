88ea1101e3abba51cd1954dbc05d8ce35c7a4ae2
hadas <hadas@e-c.co.il>
2022-01-20 17:31:32 +0200

    ECNG-1324 3d secure. MerchantUrl- RestApi address to which process results will be returned

---
+++ EasyCard/CheckoutPortal/Controllers/ThreeDSController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheckoutPortal.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Api.Models;
+
+namespace CheckoutPortal.Controllers
+{
+    [Route("api/3ds-version")]
+    [ApiController]
+    public class ThreeDSController : ControllerBase
+    {
+        [HttpPost]
+        public Task<ActionResult<OperationResponse>> VersionResponse(ThreeDSecureGetVersionReq request)
+        {
+            return null;
+        }
+    }
+}
---
+++ EasyCard/CheckoutPortal/Models/3DS/ThreeDSMethodDataForm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheckoutPortal.Models._3DS
+{
+    public class ThreeDSMethodDataForm
+    {
+        public string threeDSMethodData { get; set; }
+    }
+}
