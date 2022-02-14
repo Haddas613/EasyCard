94c19e56fc87ff1d16952a462105f0a895f2c2b5
volkovvdcode <volkovv@mydigicode.com>
2022-02-14 15:07:20 +0200

    billing deals summary report

---
+++ EasyCard/Transactions.Business/Services/IReportingService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Transactions.Business.Entities.Reporting;
+
+namespace Transactions.Business.Services
+{
+    public interface IReportingService
+    {
+        IQueryable<BillingSummaryReport> GetBillingSummaryReport(bool sharedTerminal);
+    }
+}
