6945b94cd11c2dfddb6e9df01c429667051e0679
Igor Kanivets <kanivetsig@mydigicode.com>
2022-03-02 13:04:24 +0200

    ECNG-1364: Add suport for legacy query string for Bit transactions

---
+++ EasyCard/CheckoutPortal/Services/MemoryPaymentRequestStorage.cs
@@ -0,0 +1,44 @@
+﻿using CheckoutPortal.Models;
+using System.Collections.Concurrent;
+
+namespace CheckoutPortal.Services
+{
+    /// <summary>
+    /// Temporary solution. Should store ChargeViewModel from HomeController.Charge in case it's needed afterwards.
+    /// To be injected as singleton.
+    /// </summary>
+    public class MemoryPaymentRequestStorage
+    {
+        private ConcurrentDictionary<string, ChargeViewModel> Storage { get; set; }
+
+        public MemoryPaymentRequestStorage()
+        {
+            Storage = new ConcurrentDictionary<string, ChargeViewModel>();
+        }
+
+        public void AddOrUpdate(string key, ChargeViewModel model)
+        {
+            Storage.AddOrUpdate(key, model, (k, v) =>
+            {
+                Storage.TryRemove(k, out _);
+                Storage.TryAdd(k, v);
+                return v;
+            });
+        }
+
+        public ChargeViewModel Get(string key, bool removeAfter = false)
+        {
+            if (Storage.TryGetValue(key, out ChargeViewModel model))
+            {
+                if (removeAfter)
+                {
+                    Storage.TryRemove(key, out _);
+                }
+
+                return model;
+            }
+
+            return null;
+        }
+    }
+}
