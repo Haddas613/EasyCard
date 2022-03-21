e864afdd55c507fbd5488f0dab98f1524f4d9231
Igor Kanivets <kanivetsig@mydigicode.com>
2022-03-21 10:59:44 +0100

    ECNG-1395: Improve "Redirect payment form"

--- EasyCard/CheckoutPortal/Resources/CommonResources.Designer.cs
+++ EasyCard/CheckoutPortal/Resources/CommonResources.Designer.cs
@@ -19,7 +19,7 @@ namespace CheckoutPortal.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class CommonResources {
@@ -120,6 +120,15 @@ namespace CheckoutPortal.Resources {
         public static string CardOwnerName {
             get {
                 return ResourceManager.GetString("CardOwnerName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Choose payment method.
+        /// </summary>
+        public static string ChoosePaymentMethod {
+            get {
+                return ResourceManager.GetString("ChoosePaymentMethod", resourceCulture);
             }
         }
         
