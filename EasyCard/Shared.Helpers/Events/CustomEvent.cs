bd1f51ee3968e661b4767fb9ebbbed52b582fda2
volkovvdcode <volkovv@mydigicode.com>
2022-03-23 11:13:34 +0200

    awailable webhooks
    (cherry picked from commit 2c6cead6416735fb331f33845217815d965cc1e6)

--- EasyCard/Shared.Helpers/Events/CustomEvent.cs
+++ EasyCard/Shared.Helpers/Events/CustomEvent.cs
@@ -7,6 +7,26 @@ namespace Shared.Helpers.Events
 {
     public class CustomEvent : CustomEventBase
     {
+        public const string TransactionCreated = "TransactionCreated";
+
+        public const string TransactionRejected = "TransactionRejected";
+
+        public const string CardTokenCreated = "CardTokenCreated";
+
+        public const string CardTokenDeleted = "CardTokenDeleted";
+
+        public const string BillingDealCreated = "BillingDealCreated";
+
+        public const string BillingDealUpdated = "BillingDealUpdated";
+
+        public const string InvoiceGenerated = "InvoiceGenerated";
+
+        public const string InvoiceGenerationFailed = "InvoiceGenerationFailed";
+
+        public const string ConsumerCreated = "ConsumerCreated";
+
+        public const string ConsumerUpdated = "ConsumerUpdated";
+
         [JsonIgnore]
         public object Entity { get; set; }
     }
