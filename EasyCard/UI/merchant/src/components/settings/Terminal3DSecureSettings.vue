be608298d8ee52740a74685f9aa99002bd20efab
Igor Kanivets <kanivetsig@mydigicode.com>
2022-04-14 08:59:00 +0200

    3D Secure confirmation screen Reject button fix

--- EasyCard/UI/merchant/src/components/settings/Terminal3DSecureSettings.vue
+++ EasyCard/UI/merchant/src/components/settings/Terminal3DSecureSettings.vue
@@ -8,13 +8,13 @@
             <p>{{ consentMsg }}</p>
           </v-col>
           <v-col cols="12" class="d-flex justify-end" v-if="!$vuetify.breakpoint.smAndDown">
-            <v-btn class="mx-1" color="error" @click="visible = false">{{ $t('Reject') }}</v-btn>
+            <v-btn class="mx-1" color="error" @click="showConsentDialog = false">{{ $t('Reject') }}</v-btn>
             <v-btn color="success" @click="confirm($t('AgreeAndProceed'))">{{
               $t('AgreeAndProceed')
             }}</v-btn>
           </v-col>
           <v-col class="px-2" cols="12" v-if="$vuetify.breakpoint.smAndDown">
-            <v-btn block color="error" @click="visible = false">{{ $t('Reject') }}</v-btn>
+            <v-btn block color="error" @click="showConsentDialog = false">{{ $t('Reject') }}</v-btn>
             <v-spacer class="py-2"></v-spacer>
             <v-btn block color="success" @click="confirm($t('AgreeAndProceed'))">{{
               $t('AgreeAndProceed')
