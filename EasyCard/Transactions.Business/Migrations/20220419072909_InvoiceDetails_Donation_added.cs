e683a2a34321c3d3c0d8ca300b547d914e5890e0
Igor Kanivets <kanivetsig@mydigicode.com>
2022-04-19 09:30:35 +0200

    Donation migration

---
+++ EasyCard/Transactions.Business/Migrations/20220419072909_InvoiceDetails_Donation_added.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Transactions.Business.Migrations
+{
+    public partial class InvoiceDetails_Donation_added : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "InvoiceDetails_Donation",
+                table: "PaymentRequest",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "InvoiceDetails_Donation",
+                table: "Invoice",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "InvoiceDetails_Donation",
+                table: "BillingDeal",
+                type: "bit",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InvoiceDetails_Donation",
+                table: "PaymentRequest");
+
+            migrationBuilder.DropColumn(
+                name: "InvoiceDetails_Donation",
+                table: "Invoice");
+
+            migrationBuilder.DropColumn(
+                name: "InvoiceDetails_Donation",
+                table: "BillingDeal");
+        }
+    }
+}
