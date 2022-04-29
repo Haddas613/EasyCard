486115b5e2f50b2d4492f059c59736af5fbd722f
Igor Kanivets <kanivetsig@mydigicode.com>
2022-04-29 09:25:55 +0200

    ECNG-1539: Merchant side Billing list. Date format incorrect

--- EasyCard/UI/merchant/src/extensions/filters/ecdate.js
+++ EasyCard/UI/merchant/src/extensions/filters/ecdate.js
@@ -3,10 +3,6 @@ import store from '../../store/index';
 
 export default function(value, format = null){
     if(!value) return;
-    
-    if(format == "DT"){
-        format = "DD/MM/YYYY HH:mm";
-    }
 
     if(!format){
         /**Default locale format LLLL can not be used due to time present in the output*/
@@ -14,6 +10,14 @@ export default function(value, format = null){
             format = "dddd, D MMMM YYYY";
         }else{
             format = "dddd, MMMM D, YYYY"
+        }
+    } else {
+        if(format.toLowerCase() == "dt"){
+            format = "DD/MM/YYYY HH:mm";
+        }
+    
+        if(format.toLowerCase() == "d"){
+            format = "DD/MM/YYYY";
         }
     }
     return moment.utc(value).local().locale(store.state.localization.currentLocale).format(format);
