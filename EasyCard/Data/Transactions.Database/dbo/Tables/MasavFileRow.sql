8c1b62b25776a1eb0223995a0c982792b9eaf702
volkovvdcode <volkovv@mydigicode.com>
2022-05-08 12:27:06 +0300

    ECNG-1552: fix

--- EasyCard/Data/Transactions.Database/dbo/Tables/MasavFileRow.sql
+++ EasyCard/Data/Transactions.Database/dbo/Tables/MasavFileRow.sql
@@ -3,19 +3,21 @@
     [MasavFileID]          BIGINT           NULL,
     [PaymentTransactionID] UNIQUEIDENTIFIER NULL,
     [ConsumerID]           UNIQUEIDENTIFIER NULL,
-    [Bankcode]             INT              NULL,
-    [BranchNumber]         INT              NULL,
-    [AccountNumber]        INT              NULL,
-    [NationalID]           INT              NULL,
+    [Bankcode]             BIGINT           NULL,
+    [BranchNumber]         BIGINT           NULL,
+    [AccountNumber]        BIGINT           NULL,
+    [NationalID]           BIGINT           NULL,
     [Amount]               DECIMAL (19, 4)  NULL,
     [IsPayed]              BIT              NULL,
     [SmsSent]              BIT              NOT NULL,
     [SmsSentDate]          DATETIME2 (7)    NULL,
     [ConsumerName]         NVARCHAR (50)    NULL,
-    [InstituteNumber]      INT              NULL,
+    [InstituteNumber]      BIGINT           NULL,
     CONSTRAINT [PK_MasavFileRow] PRIMARY KEY CLUSTERED ([MasavFileRowID] ASC),
     CONSTRAINT [FK_MasavFileRow_MasavFile_MasavFileID] FOREIGN KEY ([MasavFileID]) REFERENCES [dbo].[MasavFile] ([MasavFileID])
 );
+
+
 
 
 
