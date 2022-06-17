b091f40bca31019d10840787b0b47a6f35b0f0db
volkovvdcode <volkovv@mydigicode.com>
2022-06-17 19:08:16 +0300

    corrections

--- EasyCard/Data/Transactions.Database/dbo/Stored Procedures/PR_FutureBillings.sql
+++ EasyCard/Data/Transactions.Database/dbo/Stored Procedures/PR_FutureBillings.sql
@@ -1,4 +1,5 @@
-﻿CREATE PROCEDURE [dbo].[PR_FutureBillings]
+﻿
+CREATE PROCEDURE [dbo].[PR_FutureBillings]
 @TerminalID uniqueidentifier,
 @MerchantID uniqueidentifier,
 @ConsumerID uniqueidentifier,
@@ -9,7 +10,7 @@
 with execute as owner
 AS BEGIN
 
-
+select * from (
 select a.*, t.n as FutureDeal, Dateadd(mm, t.n * a.repeatPeriodType, a.NextScheduledTransaction) as FutureTransactionDate
 from (
 select 
@@ -28,7 +29,10 @@ WITH (
 where 
 b.Active = 1 and b.NextScheduledTransaction is not null and
 b.MerchantID = @MerchantID and (@TerminalID is null or b.TerminalID = @TerminalID) and (@ConsumerID is null or b.ConsumerID = @ConsumerID) and (@BillingDealID is null or b.BillingDealID = @BillingDealID)
-) a
-inner join _numbers as t on t.n > isnull(a.CurrentDeal,0)  
+) as a
+inner join _numbers as t on t.n > isnull(a.CurrentDeal,0) and (a.endAtType in (0, 1) or t.n <= a.endAtNumberOfPayments)
+) as k
+where 
+(k.endAtType in (0,2) or k.FutureTransactionDate <= k.endAt) and (@StartDate is null or k.FutureTransactionDate >= @StartDate) and (@EndDate is null or k.FutureTransactionDate <= @EndDate)
 
 END
--- EasyCard/Transactions.Api/Extensions/PaymentRequestsHelper.cs
+++ EasyCard/Transactions.Api/Extensions/PaymentRequestsHelper.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Merchants.Business.Entities.Terminal;
+using Shared.Integration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Transactions.Api.Models.PaymentRequests;
 using Transactions.Api.Models.PaymentRequests.Enums;
 using Transactions.Shared.Enums;
+using SharedIntegration = Shared.Integration;
 
 namespace Transactions.Api.Extensions
 {
@@ -42,6 +46,59 @@ namespace Transactions.Api.Extensions
             }
 
             return PayReqQuickStatusFilterTypeEnum.Pending;
+        }
+
+        public static void UpdatePaymentRequest(this PaymentRequestCreate model, Terminal terminal)
+        {
+            if (!model.IssueInvoice.HasValue && terminal.CheckoutSettings.IssueInvoice == true)
+            {
+                model.IssueInvoice = true;
+            }
+
+            if (model.IssueInvoice.GetValueOrDefault())
+            {
+                if (model.InvoiceDetails == null)
+                {
+                    model.InvoiceDetails = new SharedIntegration.Models.Invoicing.InvoiceDetails { InvoiceType = terminal.InvoiceSettings.DefaultInvoiceType.GetValueOrDefault() };
+                }
+
+                model.InvoiceDetails.UpdateInvoiceDetails(terminal.InvoiceSettings);
+            }
+
+            if (model.AllowPinPad.GetValueOrDefault())
+            {
+                model.PinPadDetails = model.PinPadDetails.UpdatePinPadDetails(terminal.Integrations.FirstOrDefault(i => i.ExternalSystemID == ExternalSystemHelpers.NayaxPinpadProcessorExternalSystemID));
+            }
+
+            if (model.AllowCredit == null && (terminal.CheckoutSettings.AllowCredit == null || terminal.CheckoutSettings.AllowCredit == true))
+            {
+                model.AllowCredit = true;
+            }
+
+            if (model.AllowInstallments == null && (terminal.CheckoutSettings.AllowInstallments == null || terminal.CheckoutSettings.AllowInstallments == true))
+            {
+                model.AllowInstallments = true;
+            }
+
+            if (model.HideEmail == null && terminal.CheckoutSettings.HideEmail == true)
+            {
+                model.HideEmail = true;
+            }
+
+            if (model.HidePhone == null && terminal.CheckoutSettings.HidePhone == true)
+            {
+                model.HidePhone = true;
+            }
+
+            if (model.HideNationalID == null && terminal.CheckoutSettings.HideNationalID == true && terminal.Settings.NationalIDRequired != true)
+            {
+                model.HideNationalID = true;
+            }
+
+            if (!model.VATRate.HasValue)
+            {
+                model.VATRate = terminal.Settings.VATRate;
+            }
         }
     }
 }
