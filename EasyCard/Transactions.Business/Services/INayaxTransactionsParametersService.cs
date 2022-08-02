8517d21cccf502675fe8988efb3a93af94359f18
hadas <hadas@e-c.co.il>
2022-08-02 19:30:17 +0300

    added update id entity is exist

--- EasyCard/Transactions.Business/Services/INayaxTransactionsParametersService.cs
+++ EasyCard/Transactions.Business/Services/INayaxTransactionsParametersService.cs
@@ -2,11 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Transactions.Business.Entities;
 
 namespace Transactions.Business.Services
 {
     public interface INayaxTransactionsParametersService : IServiceBase<NayaxTransactionsParameters, Guid>
     {
+        Task<NayaxTransactionsParameters> GetNayaxTransactionsParameter(string vuid);
+
     }
 }
--- EasyCard/Transactions.Business/Services/NayaxTransactionsParametersService.cs
+++ EasyCard/Transactions.Business/Services/NayaxTransactionsParametersService.cs
@@ -1,9 +1,11 @@
-﻿using Shared.Business;
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Business;
 using Shared.Business.Security;
 using System;
 using System.Collections.Generic;
 using System.Security.Claims;
 using System.Text;
+using System.Threading.Tasks;
 using Transactions.Business.Data;
 using Transactions.Business.Entities;
 
@@ -21,6 +23,11 @@ namespace Transactions.Business.Services
             this.context = context;
             this.httpContextAccessor = httpContextAccessor;
             user = httpContextAccessor.GetUser();
+        }
+
+        public Task<NayaxTransactionsParameters> GetNayaxTransactionsParameter(string vuid)
+        {
+            return context.NayaxTransactionsParameters.AsNoTracking().FirstOrDefaultAsync(p => p.PinPadTransactionID == vuid);
         }
     }
 }
