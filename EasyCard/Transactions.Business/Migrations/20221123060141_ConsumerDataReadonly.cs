c1cad9f6efb2f95f0319a14ddeff58ff7a6361c9
Vlad Volkov <40884416+volkovvdcode@users.noreply.github.com>
2022-11-23 08:05:20 +0200

    missed column added

---
+++ EasyCard/Transactions.Business/Migrations/20221123060141_ConsumerDataReadonly.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Transactions.Business.Migrations
+{
+    public partial class ConsumerDataReadonly : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ConsumerDataReadonly",
+                table: "PaymentRequest",
+                type: "bit",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConsumerDataReadonly",
+                table: "PaymentRequest");
+        }
+    }
+}
