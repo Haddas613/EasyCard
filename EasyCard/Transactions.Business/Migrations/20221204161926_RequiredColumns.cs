3d9a6fe5787424931f1d5884bf45f1b907a93415
Vlad Volkov <40884416+volkovvdcode@users.noreply.github.com>
2022-12-04 19:18:06 +0200

    refresh db

---
+++ EasyCard/Transactions.Business/Migrations/20221204161926_RequiredColumns.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Transactions.Business.Migrations
+{
+    public partial class RequiredColumns : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "EmailRequired",
+                table: "PaymentRequest",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "NationalIDRequired",
+                table: "PaymentRequest",
+                type: "bit",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "PhoneRequired",
+                table: "PaymentRequest",
+                type: "bit",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailRequired",
+                table: "PaymentRequest");
+
+            migrationBuilder.DropColumn(
+                name: "NationalIDRequired",
+                table: "PaymentRequest");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneRequired",
+                table: "PaymentRequest");
+        }
+    }
+}
